e858efbc4fa18a5cad0c5204d91dc208aa6c66ed
rendrap <rendrapradana@gmail.com>
2019-01-20 17:33:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,105 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{ props.title }</h1>
+      <span className='stats'>Players: { props.totalPlayers }</span>
+    </header>
+  );
+};
+
+const Player = (props) => {
+  return (
+    <div className='player'>
+      <span className='player-name'>
+      <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>✖</button>
+        { props.name }
+      </span>
+
+      <Counter />
+    </div>
+  );
+};
+
+
+class Counter extends React.Component {
+
+  state = {
+    score: 0
+  };
+
+  incrementScore = () => {
+    this.setState( prevState => ({
+      score : prevState.score + 1
+    }));
+  };
+
+  decrementScore = () => {
+    this.setState( prevState => ({
+      score : prevState.score - 1
+    }));
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='counter'>
+        <button className='counter-action decrement' onClick={() => this.decrementScore()}> - </button>
+        <span className='counter-score'>{ this.state.score }</span>
+        <button className='counter-action increment' onClick={() => this.incrementScore()}> + </button>
       </div>
     );
   }
-}
+};
+
+class App extends React.Component {
+  state = {
+    players: [
+    {
+    name: 'Bambang',
+    id: 1
+  },
+  {
+    name: 'Tina',
+    id: 2
+  },
+  {
+    name: 'Ruby',
+    id: 3
+  },
+  {
+    name: 'Eko',
+    id: 4
+  }
+    ]
+  }
+
+  handleRemovePlayer = (id) => {
+    this.setState( prevState => {
+      return{
+        players: prevState.players.filter( p => p.id !== id )
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className='scoreboard'>
+        <Header title='Scoreboard' totalPlayers={this.state.players.length} />
+
+        {/* Player list */}
+        {this.state.players.map(player =>
+          (<Player
+            name={player.name}
+            key={player.id.toString()}
+            id={player.id}
+            removePlayer={this.handleRemovePlayer}
+          />)
+        )}
+
+      </div>
+    );
+  }
+};
 
 export default App;
