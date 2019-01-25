3fb34a10906acf5544210d093c33a5fce2b14ca6
rendrap <rendrapradana@gmail.com>
2019-01-25 17:27:28 +0700

    create Stopwatch component

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Stats from './Stats.js'
+import Stopwatch from './Stopwatch.js'
 
 const Header = (props) => {
   return (
     <header>
       <Stats players={props.players} />
       <h1>{ props.title }</h1>
+      <Stopwatch />
     </header>
   );
 };
---
+++ src/Stopwatch.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Stopwatch extends Component {
+
+  state = {
+    isRunning: false
+  };
+
+  render() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">0</span>
+        <button>
+          { this.state.isRunning ? 'Stop' : 'Start' }
+        </button>
+        <button>Reset</button>
+      </div>
+    );
+  }
+};
+
+export default Stopwatch
