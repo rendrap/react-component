402d45fce3ef567615ad2ee78b2570c0829fabac
rendrap <rendrapradana@gmail.com>
2019-01-29 23:12:29 +0700

    Proptypes checking on Stats component

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Stats from './Stats.js'
 import Stopwatch from './Stopwatch.js'
 
@@ -10,6 +11,11 @@ const Header = ({ players, title }) => {
       <Stopwatch />
     </header>
   );
+};
+
+Header.proptypes = {
+  title: PropTypes.string,
+  players: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default Header;
