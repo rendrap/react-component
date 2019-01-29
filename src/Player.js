8e01a79cf9ffbde9861cd83962cdafcc80e8753b
rendrap <rendrapradana@gmail.com>
2019-01-29 11:37:44 +0700

    deconstructuring Player component

--- src/Player.js
+++ src/Player.js
@@ -4,20 +4,28 @@ import Counter from './Counter'
 class Player extends PureComponent {
   render() {
 
-    console.log(this.props.name + ' rendered');
+    const {
+      name,
+      id,
+      removePlayer,
+      score,
+      changeScore,
+      index
+    } = this.props;
+
+    console.log(name + ' rendered');
 
     return (
       <div className='player'>
         <span className='player-name'>
-        <button className="remove-player" onClick={ () => this.props.removePlayer(this.props.id) }>✖</button>
-          { this.props.name }
+        <button className="remove-player" onClick={ () => removePlayer(id) }>✖</button>
+          { name }
         </span>
 
         <Counter
-          score={this.props.score}
-          changeScore={this.props.changeScore}
-          index={this.props.index}
-
+          score={score}
+          changeScore={changeScore}
+          index={index}
         />
       </div>
     );
