1530b3328e843da5fa64af1fea0afa410ad2bf17
rendrap <rendrapradana@gmail.com>
2019-01-29 09:42:13 +0700

    deconstructuring Header component

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Stats from './Stats.js'
 import Stopwatch from './Stopwatch.js'
 
-const Header = (props) => {
+const Header = ({ players, title }) => {
   return (
     <header>
-      <Stats players={props.players} />
-      <h1>{ props.title }</h1>
+      <Stats players={players} />
+      <h1>{ title }</h1>
       <Stopwatch />
     </header>
   );
